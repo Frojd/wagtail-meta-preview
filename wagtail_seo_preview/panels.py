7fab7d4767e31001aa1aace6c73f2566b5d8e198
Andreas Bernacca <hello@rinti.se>
2020-07-25 10:43:12 +0200

    Add more twitter styling

--- wagtail_seo_preview/panels.py
+++ wagtail_seo_preview/panels.py
@@ -1,3 +1,5 @@
+from django.utils.safestring import mark_safe
+from django.template.loader import render_to_string
 from wagtail.admin.edit_handlers import BaseCompositeEditHandler
 from wagtail.admin.edit_handlers import FieldPanel
 
@@ -18,6 +20,11 @@ class SeoDescriptionPanel(FieldPanel):
 
 class TwitterPreviewPanel(BaseCompositeEditHandler):
     template = "wagtail_seo_preview/preview_panel.html"
+
+    def render(self):
+        return mark_safe(
+            render_to_string(self.template, {"self": self, "is_twitter": True})
+        )
 
     def classes(self):
         classes = super().classes()
