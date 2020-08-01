2fa44f82db5fa4e60f79cf83422edd1475486f73
Andreas Bernacca <hello@rinti.se>
2020-08-01 20:39:02 +0200

    Add test for utils

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 test:
-	python runtests.py
+	docker-compose exec web python runtests.py
 
 coverage:
-	coverage run --source "./wagtail_meta_preview" runtests.py && coverage report && coverage html
+	docker-compose exec web coverage run --source "./wagtail_meta_preview" runtests.py && coverage report && coverage html
 
 format_code:
-	black --exclude "/(\.eggs|\.git|\.hg|\.mypy_cache|\.nox|\.tox|\.venv|_build|buck-out|build|dist|migrations)/" ./
+	docker-compose exec web black --exclude "/(\.eggs|\.git|\.hg|\.mypy_cache|\.nox|\.tox|\.venv|_build|buck-out|build|dist|migrations)/" ./
---
+++ tests/test_utils.py
@@ -0,0 +1,27 @@
+from io import BytesIO
+
+import PIL.Image
+from django.test import TestCase
+from django.core.files.images import ImageFile
+from wagtail.tests.utils import WagtailTestUtils
+from wagtail.images.models import Image
+
+from wagtail_meta_preview.utils import get_focal
+
+
+# Taken from wagtail.images.test.utils
+def get_test_image_file(filename="test.png", colour="white", size=(640, 480)):
+    f = BytesIO()
+    image = PIL.Image.new("RGBA", size, colour)
+    image.save(f, "PNG")
+    return ImageFile(f, name=filename)
+
+
+class TestUtils(TestCase, WagtailTestUtils):
+    def setUp(self):
+        self.image = Image.objects.create(
+            title="Test image", file=get_test_image_file(colour="white"),
+        )
+
+    def test_focal(self):
+        self.assertEqual(get_focal(self.image), {"x": "50.00%", "y": "50.00%"})
